a96b3aa2900d4826f3b11f247fce65440b3556a8
Toma <Thomas.officer@gmail.com>
2023-10-25 13:03:37 -0400

    welp

--- main.py
+++ main.py
@@ -36,14 +36,16 @@ def logout():
 
 @app.route('/login/authorized')
 def authorized():
+    print("Authorized function triggered.")  # Debugging line
     response = github.authorized_response()
+    print("GitHub Response:", response)  # Debugging line
     if response is None or response.get('access_token') is None:
         error_reason = request.args.get('error_reason', 'No reason provided')
         error_description = request.args.get('error_description', 'No description provided')
         return f'Access denied: reason={error_reason} error={error_description}'
     
     session['github_token'] = (response['access_token'], '')
-    return redirect(url_for('authorized'))
+    return render_template('authorized.html', token=session.get('github_token'))
 
 @github.tokengetter
 def get_github_oauth_token():
