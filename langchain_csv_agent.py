28d3df6e68429c0907bb66281165c39f6fdcf82a
Toma <Thomas.officer@gmail.com>
2023-11-05 17:27:45 -0500

    working agent, but deprecated langchain function

--- langchain_csv_agent.py
+++ langchain_csv_agent.py
@@ -1,6 +1,22 @@
-import pandas as pd
+# import pandas as pd
+from langchain.llms import OpenAI
+from langchain.agents.agent_types import AgentType
+from langchain.agents import create_csv_agent
+import os
+from dotenv import load_dotenv
 
-def initialize_langchain_csv_agent():
-  df = pd.read_csv('example_data/budget-breakdown.csv')
-  df.head()
-  print(df)
+load_dotenv()
+
+agent=create_csv_agent(
+  llm=OpenAI(temperature=0), 
+  path='example_data/budget-breakdown.csv',
+  agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
+  )
+print(agent.run("How much has been spent on groceries so far?"))
+
+
+# def initialize_langchain_csv_agent():
+#   # Quick test with pandas to see .csv
+#   df = pd.read_csv('example_data/budget-breakdown.csv')
+#   df.head()
+#   print(df)
