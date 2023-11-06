8c0a571d49d09c58fcc6ae9cbe028c04abcb2bc4
Toma <Thomas.officer@gmail.com>
2023-11-06 14:17:25 -0500

    comment out unnecessary agent tool

--- langchain_agent.py
+++ langchain_agent.py
@@ -3,12 +3,12 @@ from langchain.agents import initialize_agent
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.chains import LLMMathChain
-from langchain_csv_agent import csv_agent_query_function 
+# from langchain_csv_agent import csv_agent_query_function 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatOpenAI(temperature=0, model="gpt-4")
+llm = ChatOpenAI(temperature=0.01, model="gpt-4", max_tokens=3600)
 llm_math = LLMMathChain.from_llm(llm)
 
 tools = [
@@ -18,22 +18,24 @@ tools = [
     "Useful for when you need to do math or calculate expressions. Please provide an expression.",
     func=llm_math.run,
     return_direct=True),
-  Tool(
-      name="October 2023 expenses",
-      description="Searches for data pertaining to October 2023 expenses.",
-      func=lambda q: csv_agent_query_function(q),  # Pass the function as a lambda to delay execution
-      return_direct=True
-  )
+  # Tool(
+  #     name="October 2023 expenses",
+  #     description="Searches for data pertaining to October 2023 expenses.",
+  #     func=lambda q: csv_agent_query_function(q),  # Pass the function as a lambda to delay execution
+  #     return_direct=True
+  # )
 ]
 
 memory = ConversationBufferMemory(memory_key="chat_history")
 agent_chain = initialize_agent(tools,
                                llm,
                                agent="conversational-react-description",
+                               verbose=True,
+                               handle_parsing_errors=True,
                                memory=memory)
 
 # Hardcoded question for the CSV agent
-hardcoded_question = "What is the total?"
+hardcoded_question = "What are the top three categories of expense that you see in the data? Please just give me the category names that you come up with."
 
 # Run the main agent chain with the hardcoded question
 answer = agent_chain.run(input=hardcoded_question)
--- langchain_csv_agent.py
+++ langchain_csv_agent.py
@@ -1,6 +1,13 @@
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 from langchain.agents.agent_types import AgentType
-from langchain.agents import create_csv_agent
+from langchain_experimental.agents.agent_toolkits.csv.base import create_csv_agent
 from dotenv import load_dotenv
 
 # Load the environment variables
@@ -10,15 +17,18 @@ load_dotenv()
 def csv_agent_query_function(question, csv_path='example_data/October2023_3780.csv'):
     # Create the CSV agent
     csv_agent = create_csv_agent(
-        llm=OpenAI(temperature=0), 
+        llm=ChatOpenAI(temperature=0.01, model="gpt-4", max_tokens=3600), 
         path=csv_path,
+        verbose=True,
         agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION
     )
+    # Set the handle_parsing_errors attribute
+    csv_agent.handle_parsing_errors = True
     # Run the agent with the question
     return csv_agent.run(question)
 
 # # Hardcoded question
-# hardcoded_question = "How much has been spent on food related items?"
+# hardcoded_question = "What are the top three categories of expense that you see in the data? Please just give me the category names that you come up with."
 
 # # Get the answer to the hardcoded question
 # answer = csv_agent_query_function(hardcoded_question)
