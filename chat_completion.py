304472aac673e535fbe51b6f4975821c3b5e74c8
Toma <Thomas.officer@gmail.com>
2023-11-09 19:50:27 -0500

    loop chat completion

--- chat_completion.py
+++ chat_completion.py
@@ -1,15 +1,31 @@
+import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+def main():
+    while True:
+        # Get user input
+        user_input = input("Ask your poetic assistant: ")
 
-print(completion.choices[0].message)
+        # Check if the user wants to exit
+        if user_input.lower() == 'exit':
+            print("Goodbye!")
+            break
+
+        # Create the completion request
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+                {"role": "user", "content": user_input}
+            ]
+        )
+
+        # Print the response
+        print(completion.choices[0].message.content)
+
+if __name__ == "__main__":
+    main()
